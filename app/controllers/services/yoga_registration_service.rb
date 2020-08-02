120282329014e8420318cc30aaf0f09be28a720b
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-08-02 20:40:57 +0900

    YogaRegistrationServiceの実装

---
+++ app/controllers/services/yoga_registration_service.rb
@@ -0,0 +1,34 @@
+require 'date'
+
+class YogaRagistrationService
+  def reminder(today)
+    day_of_week = today.wday
+    week_of_month = get_week_of_month(today)
+
+    case day_of_week
+      when 0
+        "ところでヨガの次の予約した？"
+      when 3
+        "ストレッチだけでもしとこっか？"
+      else
+        ''
+      end
+  end
+
+  private
+
+  def get_week_of_month(today)
+    first_week = (today - (today.day - 1)).cweek
+    this_week = today.cweek
+  
+    # 年末の暦週調整
+    if this_week < first_week
+      if today.month == 12
+        return get_week_of_month(today - 7) + 1
+      else
+        return this_week + 1
+      end
+    end
+    return this_week - first_week + 1
+  end
+end
