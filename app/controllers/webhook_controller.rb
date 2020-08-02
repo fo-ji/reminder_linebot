083890f4ca9920565c11447bcb4ee9343a52e7bd
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-08-02 14:21:55 +0900

    webhookコントローラーの設定

---
+++ app/controllers/webhook_controller.rb
@@ -0,0 +1,32 @@
+class WebhookController < ApplicationController
+  #API 機能の場合はCSRFの対策は必要なし
+  protect_from_forgey with: :null_session
+
+  def reminder
+    reminders = get_reminders
+    logger.info(reminders)
+    #文字列をログファイルに書き出す
+
+    reminders.each do |reminder|
+      res = line_client.push(reminder)
+      logger.info(res.body)
+      #line_clientsにpushした文字列をログファイルに書き出す
+    end
+  end
+
+  #クラス内のみアクセス
+  private
+  
+  def get_reminders
+    reminder_service.execute
+    #ReminderServiceをnewして、reminder_service.rbのexecuteを呼び出し
+  end
+
+  def reminder_service
+    ReminderService.new
+  end
+
+  def line_client
+    @line_clinet ||= LineClient.new
+  end
+end
